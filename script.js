4a5da08ba5d91b9a50e5e717579f879b603b9b97
lautaromartinez04 <lautarom403@gmail.com>
2024-06-14 12:08:07 -0300

    leves cambios

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ donemilio.addEventListener('mouseover', () => {
     donemilio.style.boxShadow = '0px 0px 10px 0px #0033a1';
     marcadonemilio.style.boxShadow = '0px 0px 10px 0px #0033a1';
     linea.style.backgroundColor = '#0033a1';
-    duyamis.style.filter = 'blur(2px)';
-    mharnes.style.filter = 'blur(2px)';
+    duyamis.style.filter = 'grayscale(0.3)';
+    mharnes.style.filter = 'grayscale(0.3)';
 });
 
 duyamis.addEventListener('mouseover', () => {
@@ -26,8 +26,8 @@ duyamis.addEventListener('mouseover', () => {
     duyamis.style.boxShadow = '0px 0px 10px 0px #E32515';
     marcaduyamis.style.boxShadow = '0px 0px 10px 0px #E32515';
     linea.style.backgroundColor = '#E32515';
-    donemilio.style.filter = 'blur(2px)';
-    mharnes.style.filter = 'blur(2px)';
+    donemilio.style.filter = 'grayscale(0.3)';
+    mharnes.style.filter = 'grayscale(0.3)';
 });
 
 mharnes.addEventListener('mouseover', () => {
@@ -37,8 +37,8 @@ mharnes.addEventListener('mouseover', () => {
     mharnes.style.boxShadow = '0px 0px 10px 0px #1D71B8';
     marcamharnes.style.boxShadow = '0px 0px 10px 0px #1D71B8';
     linea.style.backgroundColor = '#1D71B8';
-    donemilio.style.filter = 'blur(2px)';
-    duyamis.style.filter = 'blur(2px)';
+    donemilio.style.filter = 'grayscale(0.3)';
+    duyamis.style.filter = 'grayscale(0.3)';
 });
 
 const articles = document.querySelectorAll('.GrupoDonEmilio article');
